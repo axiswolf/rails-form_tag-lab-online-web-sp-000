8b79be135f5c54415c7c86098ab0eaf561110344
Karen V <karenvo@usc.edu>
2019-11-29 08:39:34 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,4 +10,9 @@ class StudentsController < ApplicationController
 
   def new
   end
+
+  def create
+    Student.create(first_name: params[:students][:first_name], last_name: params[:students][:last_name])
+    redirect_to students_path
+  end
 end
